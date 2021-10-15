045de89742d6e2b0e839cc16785a5e2d8d8e7da6
Vinci <louwenqi@mail.ustc.edu.cn>
2021-10-15 14:52:24 +0800

    Update answer.cpp

--- host/answer.cpp
+++ host/answer.cpp
@@ -1,7 +1,7 @@
 #include "answer.hpp"
 #include <sys/time.h>
 
-#include "event_timer.cpp"
+#include "event_timer.hpp"
 /*
 void fft_test(std::string xclbin_path, DTYPE In_R[SIZE], DTYPE In_I[SIZE],DTYPE Out_R[SIZE],DTYPE Out_I[SIZE]) {
     EventTimer et;
